398ba252f35ee2d4aa0c35cbd4e3a25f16d1dd05
Anshu <singhanshu756@gmail.com>
2023-07-25 16:36:37 +0530

    update naming convention

--- controller/actorController.js
+++ controller/actorController.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 router.get('/details', async(req,res)=>{
     try{
-        const fetchusers = await service.getUser();
-        res.json(fetchusers);
+        const fetchUsers = await service.getUser();
+        res.json(fetchUsers);
     }
     catch(error){
         console.error('error while retrieving the data',error);
@@ -20,11 +20,11 @@ router.put('/details/:actor_id', async(req,res)=>{
     try{
         const {actor_id} = req.params;
         const {first_name} = req.body;
-        const updatedactor = await service.updateUser(actor_id, first_name);
-        if(!updatedactor){
+        const updateActor = await service.updateUser(actor_id, first_name);
+        if(!updateActor){
            console.log("actor id is invalid");
         }
-        res.json(updatedactor);
+        res.json(updateActor);
       } 
       catch (error) {
         console.error('Error while updating record:', error);
@@ -35,8 +35,8 @@ router.put('/details/:actor_id', async(req,res)=>{
 router.post('/create', async(req,res)=>{
     try{
         const {first_name,last_name} = req.body;
-        const newactor = await service.createUser(first_name, last_name);
-        res.json(newactor);
+        const createActor = await service.createUser(first_name, last_name);
+        res.json(createActor);
     }
     catch(error){
         console.error('Error ehile creating record:', error);
@@ -49,11 +49,11 @@ router.post('/create', async(req,res)=>{
 router.delete('/remove/:actor_id', async(req,res)=>{
     try{
         const{actor_id} = req.params;
-        const deleteactor = await service.removeUser(actor_id);
-        if(!deleteactor){
+        const removeActor = await service.removeUser(actor_id);
+        if(!removeActor){
             console.log("actor_id is invalid");
         }
-        res.json(deleteactor);
+        res.json(removeActor);
     }
     catch(error){
         console.error('erroe while deleting the record',error);
--- service/actorService.js
+++ service/actorService.js
@@ -2,29 +2,29 @@ const db = require('../connector/db');
 
 
 async function createUser(first_name, last_name){
-    const insertquery = ' INSERT INTO actor (first_name, last_name) VALUES ($1,$2) RETURNING * ';
+    const insertQuery = ' INSERT INTO actor (first_name, last_name) VALUES ($1,$2) RETURNING * ';
     const values = [first_name, last_name];
-    const result = await db.query(insertquery, values);
+    const result = await db.query(insertQuery, values);
     return result.rows[0];
 }
 
 async function getUser(){
-    const getquery = 'SELECT * FROM actor';
-    const result = await db.query(getquery);
+    const getQuery = 'SELECT * FROM actor';
+    const result = await db.query(getQuery);
     return result.rows;
 }
 
 async function updateUser(actor_id, first_name){
-    const updatequery = ' UPDATE actor SET first_name = $1 WHERE actor_id = $2 RETURNING * ';
+    const updateQuery = ' UPDATE actor SET first_name = $1 WHERE actor_id = $2 RETURNING * ';
     const values = [first_name, actor_id];
-    const result = await db.query(updatequery, values);
+    const result = await db.query(updateQuery, values);
     return result.rows[0];
 }
 
 async function removeUser(actor_id){
-    const removequery = ' DELETE FROM actor WHERE actor_id = $1 RETURNING * ';
+    const removeQuery = ' DELETE FROM actor WHERE actor_id = $1 RETURNING * ';
     const values = [actor_id];
-    const result = await db.query(removequery, values);
+    const result = await db.query(removeQuery, values);
     return result.rows[0];
 }
 
